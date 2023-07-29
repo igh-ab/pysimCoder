be5aca3e538212af3c56e98d187413c1472eb64e
Andreas Stewering-Bone <andreas.stewering-bone@gmx.de>
2023-07-29 13:06:16 +0200

    Running pdsev example

--- CodeGen/src/linux_main_rt_etherlab.c
+++ CodeGen/src/linux_main_rt_etherlab.c
@@ -1,25 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <sched.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <fcntl.h>
-#include <pthread.h>
-
-#ifdef CG_WITH_IOPL
-#include <sys/io.h>
-#endif
-
-#ifdef PDSERV
 #include <pdserv.h>
-#endif
 
-#ifdef CANOPEN
-void canopen_synch(void);
-#endif
+#include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
+#include <getopt.h>     // getopt()
+#include <unistd.h>     // getopt()
+#include <string.h>     // memset(), strcmp(), strerror()
+#include <time.h>       // clock_gettime(), clock_nanosleep()
+#include <sys/mman.h>   // mlockall()
+#include <sched.h>      // sched_setscheduler()
+#include <stdlib.h>     // strtoul(), exit()
+#include <pthread.h>    // pthread_mutex_lock(), pthread_mutex_unlock()
+
+/****************************************************************************/
+
+#define MAX_SAFE_STACK (8 * 1024) /** The maximum stack size which is
+                                    guranteed safe to access without faulting.
+                                   */
+
+#define NSEC_PER_SEC (1000000000)
+#define USEC_PER_SEC (1000000)
+#define DIFF_NS(A, B) (((long long) (B).tv_sec - (A).tv_sec) * NSEC_PER_SEC \
+        + (B).tv_nsec - (A).tv_nsec)
+
+/****************************************************************************/
 
 #define XNAME(x,y)  x##y
 #define NAME(x,y)   XNAME(x,y)
@@ -29,55 +33,55 @@ int NAME(MODEL,_isr)(double);
 int NAME(MODEL,_end)(void);
 double NAME(MODEL,_get_tsamp)(void);
 
-#define NSEC_PER_SEC    1000000000
-#define USEC_PER_SEC	1000000
+/****************************************************************************/
 
-static volatile int end = 0;
+struct pdserv* pdserv;
+struct pdtask* pdtask;
 static double T = 0.0;
 static double Tsamp;
 
-/* Options presettings */
-static char rtversion[] = "0.9";
-static int prio = 99;
-static int verbose = 0;
-static int wait = 0;
-static int extclock = 0;
-double FinalTime = 0.0;
+/* Command-line option variables.  */
 
-#ifdef PDSERV
-struct pdserv* pdserv = NULL;
-struct pdtask* pdtask = NULL;
-unsigned int overruns = 0;
+int priority = -1;      /**< Task priority, -1 means RT (maximum). */
+int daemonize = 0;      /**< Become a daemon. */
+unsigned int duration_ns = 0;
+const char *config = 0;
 
-#define MAX_SAFE_STACK (8 * 1024) /** The maximum stack size which is
-                                    guranteed safe to access without faulting.
-                                   */
-#define DIFF_NS(A, B) (((long long) (B).tv_sec - (A).tv_sec) * NSEC_PER_SEC \
-        + (B).tv_nsec - (A).tv_nsec)
+/***************************************************************************
+ * Support functions
+ ***************************************************************************/
+
+/** Increment the time.
+ * Arguments:
+ *  - t: timespec pointer
+ *  - dt_ns: increment in nanoseconds
+ */
+struct timespec* timer_add(struct timespec *t, unsigned int dt_ns)
+{
+    t->tv_nsec += dt_ns;
+    while (t->tv_nsec >= NSEC_PER_SEC) {
+        t->tv_nsec -= NSEC_PER_SEC;
+        t->tv_sec++;
+    }
+    return t;
+}
 
 /** Return the current system time.
+ *
  * This is a callback needed by pdserv.
  */
-
-int pd_gettime(struct timespec *time)
-
+int gettime(struct timespec *time)
 {
-
     return clock_gettime(CLOCK_REALTIME, time);
-
 }
 
-/** Cause a stack fault before entering cyclic operation.
- */
-void stack_prefault(void)
+static inline double calcdiff(struct timespec t1, struct timespec t2)
 {
-    unsigned char dummy[MAX_SAFE_STACK];
-
-    memset(dummy, 0, MAX_SAFE_STACK);
+  long diff;
+  diff = USEC_PER_SEC * ((long) t1.tv_sec - (long) t2.tv_sec);
+  diff += ((int) t1.tv_nsec - (int) t2.tv_nsec) / 1000;
+  return (1e-6*diff);
 }
-
-
-#endif
 
 double get_run_time(void)
 {
@@ -89,299 +93,225 @@ double get_Tsamp(void)
   return(Tsamp);
 }
 
-int get_priority_for_com(void)
+/** Cause a stack fault before entering cyclic operation.
+ */
+void stack_prefault(void)
 {
-  if (prio < 0)
-    {
-      return -1;
+    unsigned char dummy[MAX_SAFE_STACK];
+
+    memset(dummy, 0, MAX_SAFE_STACK);
+}
+
+/** Output the usage.
+ */
+void usage(FILE *f, const char *base_name)
+{
+    fprintf(f,
+            "Usage: %s [OPTIONS]\n"
+            "Options:\n"
+            "  --duration       -d secs    Set duration <float>\n"
+            "  --config         -c conffile Set configuration file\n"
+            "  --priority       -p <PRIO>  Set task priority. Default: RT.\n"
+            "  --help           -h         Show this help.\n",
+            base_name);
+}
+
+/** Get the command-line options.
+ */
+void get_options(int argc, char **argv)
+{
+    int c, arg_count;
+
+    static struct option longOptions[] = {
+        //name,           has_arg,           flag, val
+        {"duration",      required_argument, NULL, 'd'},
+        {"config",        required_argument, NULL, 'c'},
+        {"priority",      required_argument, NULL, 'p'},
+        {"help",          no_argument,       NULL, 'h'},
+        {NULL,            no_argument,       NULL,   0}
+    };
+
+    do {
+        c = getopt_long(argc, argv, "d:c:p:h", longOptions, NULL);
+
+        switch (c) {
+            case 'p':
+                if (!strcmp(optarg, "RT")) {
+                    priority = -1;
+                } else {
+                    char *end;
+                    priority = strtoul(optarg, &end, 10);
+                    if (!*optarg || *end) {
+                        fprintf(stderr, "Invalid priority: %s\n", optarg);
+                        exit(1);
+                    }
+                }
+                break;
+
+            case 'd':
+                duration_ns = atof(optarg) * NSEC_PER_SEC;
+                break;
+
+            case 'c':
+                config = optarg;
+                break;
+
+            case 'h':
+                usage(stdout, argv[0]);
+                exit(0);
+
+            case '?':
+                usage(stderr, argv[0]);
+                exit(1);
+
+            default:
+                break;
+        }
     }
-  else
-    {
-      return prio - 1;
+    while (c != -1);
+
+    arg_count = argc - optind;
+
+    if (arg_count) {
+        fprintf(stderr, "%s takes no arguments!\n", argv[0]);
+        usage(stderr, argv[0]);
+        exit(1);
     }
 }
 
-static inline void tsnorm(struct timespec *ts)
+
+
+/****************************************************************************
+ * Main function 
+ ****************************************************************************/
+int main(int argc, char **argv)
 {
-  while (ts->tv_nsec >= NSEC_PER_SEC) {
-    ts->tv_nsec -= NSEC_PER_SEC;
-    ts->tv_sec++;
-  }
-}
+    unsigned int tsample_ns = (uint64_t)(0.01e9);   // 10ms
+    const char* err = NULL;
+    int running = 1;
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    double exec_time, cycle_time;
+    unsigned int overruns = 0;
+    struct timespec monotonic_time, world_time;
+    struct timespec start_time, stop_time = {0,0}, end_time, last_start_time;
+    struct timespec t_current, T0;
+ 
+    get_options(argc, argv);
 
-static inline double calcdiff(struct timespec t1, struct timespec t2)
-{
-  long diff;
-  diff = USEC_PER_SEC * ((long) t1.tv_sec - (long) t2.tv_sec);
-  diff += ((int) t1.tv_nsec - (int) t2.tv_nsec) / 1000;
-  return (1e-6*diff);
-}
+    Tsamp = NAME(MODEL,_get_tsamp)();
 
-static void *rt_task(void *p)
-{
-  struct timespec t_next, t_current, t_isr, T0;
-#ifdef PDSERV
-  struct timespec start_time, last_start_time;
-  struct timespec end_time;
-  double cycle_time, exec_time;
+    /////////////////////// setup pdserv //////////////////////////////
 
-#endif
+    /* Create a pdserv instance */
+    if (!(pdserv = pdserv_create("PdServ Test", "1.234", gettime))) {
+        err = "Failed to init pdserv.";
+        goto out;
+    }
 
-  Tsamp = NAME(MODEL,_get_tsamp)();
+    if (config)
+        pdserv_config_file(pdserv, config);
 
-  t_isr.tv_sec =  0L;
-  t_isr.tv_nsec = (long)(1e9*Tsamp);
-  tsnorm(&t_isr);
+    /* Create a task */
+    if (!(pdtask = pdserv_create_task(pdserv, Tsamp, NULL))) {
+        err = "Failed to create task.";
+        goto out;
+    }
 
-  T=0;
+ 
+    NAME(MODEL,_init)();
 
-  NAME(MODEL,_init)();
-
-#ifdef PDSERV
-  {
     int ret = pdserv_prepare(pdserv);
     if (ret) {
-        print("Failed to prepare pdserv.");
-        exit(1);
-    }
-  }
-#endif
-
-  mlockall(MCL_CURRENT | MCL_FUTURE);
-
-  /* Provoke the first stack fault before cyclic operation. */
-  stack_prefault();
-
-
-  struct sched_param param;
-
-  if (prio >= 0) {
-    param.sched_priority = prio;
-    if(sched_setscheduler(0, SCHED_FIFO, &param)==-1) {
-      perror("sched_setscheduler failed");
-    }
-  }
-
-
-#ifdef CANOPEN
-  canopen_synch();
-#endif
-  
-  /* get current time */
-  clock_gettime(CLOCK_MONOTONIC,&t_current);
-  T0 = t_current;
-
-#ifdef PDSERV
-  last_start_time = t_current;
-#endif
-
-  while(!end){
-
-    /* periodic task */
-    T = calcdiff(t_current,T0);
-#ifdef PDSERV
-    clock_gettime(CLOCK_MONOTONIC,&start_time);
-#endif
-    NAME(MODEL,_isr)(T);
-#ifdef PDSERV
-    clock_gettime(CLOCK_MONOTONIC,&t_current);
-#endif
-
-#ifdef CANOPEN
-    canopen_synch();
-#endif
-
-    if((FinalTime >0) && (T >= FinalTime)) break;
-
-    t_next.tv_sec = t_current.tv_sec + t_isr.tv_sec;
-    t_next.tv_nsec = t_current.tv_nsec + t_isr.tv_nsec;
-    tsnorm(&t_next);
-
-    /* Check if Overrun */
-    clock_gettime(CLOCK_MONOTONIC,&t_current);
-    if (t_current.tv_sec > t_next.tv_sec ||
-	(t_current.tv_sec == t_next.tv_sec && t_current.tv_nsec > t_next.tv_nsec)) {
-      int usec = (t_current.tv_sec - t_next.tv_sec) * 1000000 + (t_current.tv_nsec -
-								 t_next.tv_nsec)/1000;
-      fprintf(stderr, "Base rate overrun by %d us\n", usec);
-      t_next= t_current;
+        err = "Failed to prepare pdserv.";
+        goto out;
     }
 
-#ifdef PDSERV
-    /* Call at end of calculation task, so that pdserv updates itself */
-    pdserv_update(pdtask, &t_current);
-    /* Calculate timing statistics */
-    cycle_time = 1.0e-9 * DIFF_NS(last_start_time, start_time);
-    exec_time  = 1.0e-9 * DIFF_NS(last_start_time, end_time);
-    last_start_time = start_time;
-    pdserv_update_statistics(pdtask, exec_time, cycle_time, overruns);
-    clock_gettime(CLOCK_MONOTONIC, &end_time);
-    overruns += DIFF_NS(t_next, end_time) > 0;
-#endif
+    /* Lock all memory forever - prevents it from being swapped out. */
+    if (mlockall(MCL_CURRENT | MCL_FUTURE))
+        fprintf(stderr, "mlockall() failed: %s\n", strerror(errno));
 
-    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t_next, NULL);
-    t_current = t_next;
-  }
-  NAME(MODEL,_end)();
-  pthread_exit(0);
-}
+    /* Provoke the first stack fault before cyclic operation. */
+    stack_prefault();
 
-void endme(int n)
-{
-  end = 1;
-}
-
-void print_usage(void)
-{
-  puts(  "\nUsage:  'RT-model-name' [OPTIONS]\n"
-         "\n"
-         "OPTIONS:\n"
-         "  -h  print usage\n"
-	 "  -f <final time> set the final time of the execution\n"
-	 "  -v  verbose output\n"
-	 "  -p <priority>  set rt task priority (default 99)\n"
-	 "  -e  external clock\n"
-	 "  -w  wait to start\n"
-	 "  -V  print version\n"
-   "  -D  command line parameters\n"
-   "        SHV_BROKER=hostname:port\n"
-	 "\n");
-}
-
-char *parse_string(char ** str, int parse_char)
-{
-  char *p = strdup(*str);
-  char *r = p;
-  char *s = strchr(r, parse_char);
-  char *t = r;
-
-  if (s == NULL)
+    /* Set task priority and scheduler. */
     {
-      return p;
+        struct sched_param param = {
+            .sched_priority = (priority == -1
+                    ? sched_get_priority_max(SCHED_FIFO)
+                    : priority),
+        };
+
+        if (sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
+            fprintf(stderr,
+                    "Setting SCHED_FIFO with priority %i failed: %s\n",
+                    param.sched_priority, strerror(errno));
+
+            /* Reset priority, so that sub-threads start */
+            priority = -1;
+        }
     }
 
-  *s = '\0';
-  r = s + 1;
+    ///////////////////// cyclic task //////////////////////////////////
 
-  *str = r;
-
-  return t;
-}
-
-static void proc_opt(int argc, char *argv[])
-{
-  int i;
-  char *t;
-
-  while((i=getopt(argc,argv,"D:ef:hp:vVw"))!=-1){
-    switch(i){
-    case 'h':
-      print_usage();
-      exit(0);
-      break;
-    case 'p':
-      prio = atoi(optarg);
-      break;
-    case 'v':
-      verbose = 1;
-      break;
-    case 'e':
-      extclock = 1;
-      break;
-    case 'w':
-      wait = 1;
-      break;
-    case 'V':
-      printf("Version %s\n",rtversion);
-      exit(0);
-      break;
-    case 'f':
-      if (strstr(optarg, "inf")) {
-        FinalTime = 0.0;
-      } else if ((FinalTime = atof(optarg)) <= 0.0) {
-        printf("-> Invalid final time.\n");
-        exit(1);
-      }
-      break;
-    case 'D':
-      t = parse_string(&optarg, '=');
-      if (t == NULL)
-        {
-          break;
-        }
-      if (strcmp(t, "SHV_BROKER") == 0)
-        {
-          t = parse_string(&optarg, ':');
-          if (t == NULL)
-            {
-              break;
-            }
-          setenv("SHV_BROKER_IP", t, 1);
-          t = parse_string(&optarg, ':');
-          if (t == NULL)
-            {
-              break;
-            }
-          setenv("SHV_BROKER_PORT", t, 1);
-        }
-      else
-        {
-          setenv(t, optarg, 1);
-        }
-      break;
+    clock_gettime(CLOCK_MONOTONIC, &monotonic_time);
+    last_start_time = monotonic_time;
+    if (duration_ns) {
+        stop_time = last_start_time;
+        timer_add(&stop_time, duration_ns);
     }
-  }
-}
 
-int main(int argc,char** argv)
-{
-  pthread_t thrd;
-  int fd;
-  int uid;
+    clock_gettime(CLOCK_MONOTONIC,&T0);
 
-  proc_opt(argc, argv);
+    while (running && (!duration_ns || DIFF_NS(last_start_time, stop_time) > 0)) {
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+        clock_gettime(CLOCK_REALTIME, &world_time);
 
-  signal(SIGINT,endme);
-  signal(SIGKILL,endme);
+        /* Get a read lock on parameters and a write lock on signals */
+        pthread_mutex_lock(&mutex);
 
-#ifdef CG_WITH_NRT
-  uid = geteuid();
-  if (uid!=0){
-    fd=open("/dev/nrt",O_RDWR);
-    if (fd<0){
-      printf("This SW needs superuser privilegies to run!\n");
-      exit(1);
+        clock_gettime(CLOCK_MONOTONIC,&t_current);
+
+        T = calcdiff(t_current,T0);
+
+        NAME(MODEL,_isr)(T);
+
+        /* Release locks */
+        pthread_mutex_unlock(&mutex);
+
+        /* Call at end of calculation task, so that pdserv updates itself */
+        pdserv_update(pdtask, &world_time);
+
+        /* Calculate timing statistics */
+        cycle_time = 1.0e-9 * DIFF_NS(last_start_time, start_time);
+        exec_time  = 1.0e-9 * DIFF_NS(last_start_time, end_time);
+        last_start_time = start_time;
+        pdserv_update_statistics(pdtask, exec_time, cycle_time, overruns);
+
+        timer_add(&monotonic_time, tsample_ns);  // Increment timer
+
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+
+        overruns += DIFF_NS(monotonic_time, end_time) > 0;
+
+        /* Wait for next cycle */
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &monotonic_time, 0);
     }
-    close(fd);
-  }
-#endif /*CG_WITH_NRT*/
 
-#ifdef CG_WITH_IOPL
-  iopl(3);
-#endif /*CG_WITH_IOPL*/
+    ///////////////////// clean up /////////////////////////////////////
 
-#ifdef PDSERV
-  /* Create a pdserv instance */
-  if (!(pdserv = pdserv_create("pysimcoder", "1.234", pd_gettime))) {
-    printf("Failed to init pdserv.");
-    exit(1);
-  }
-  /* Create a task */
-  if (!(pdtask = pdserv_create_task(pdserv, 1.0e-9*NAME(MODEL,_get_tsamp)(), NULL))) {
-    printf("Failed to create task.");
-    exit(1);
-  }
-#endif
+    NAME(MODEL,_end)();
 
-  pthread_create(&thrd,NULL,rt_task,NULL);
+    /* Clean up */
+    pdserv_exit(pdserv);
 
-  pthread_join(thrd,NULL);
+    pthread_mutex_destroy(&mutex);
 
-#ifdef PDSERV
-  /* Clean up */
-  pdserv_exit(pdserv);
-#endif
-
-  return(0);
+out:
+    if (err) {
+        fprintf(stderr, "Fatal error: %s\n", err);
+        return 1;
+    }
+    return 0;
 }
 
+/****************************************************************************/
